1131a11c82611ae01a81d80727b664ece71ce091
xloypaypa <245532675@qq.com>
2016-01-06 14:33:06 +0800

    save past ip and port

--- app/src/main/java/com/wallet/xlo/walletforandroid/ConnectingActivity.java
+++ app/src/main/java/com/wallet/xlo/walletforandroid/ConnectingActivity.java
@@ -1,6 +1,9 @@
 package com.wallet.xlo.walletforandroid;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -25,9 +28,18 @@ public class ConnectingActivity extends AbstractActivity {
         port = (EditText) this.findViewById(R.id.connectPort);
         encrypt = (CheckBox) this.findViewById(R.id.connectIsEncrypt);
 
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        ip.setText(sharedPreferences.getString("ip", ""));
+        port.setText(sharedPreferences.getInt("port", 8080) + "");
+        encrypt.setChecked(sharedPreferences.getBoolean("encrypt", false));
+
         connect.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("CommitPrefEdits")
             @Override
             public void onClick(View v) {
+                sharedPreferences.edit().putString("ip", ip.getText().toString()).commit();
+                sharedPreferences.edit().putInt("port", Integer.parseInt(port.getText().toString())).commit();
+                sharedPreferences.edit().putBoolean("encrypt", encrypt.isChecked()).commit();
                 EncryptionConfig.getEncryptionConfig().setEncryption(encrypt.isChecked());
                 controlBind.startServer(ip.getText().toString(), Integer.parseInt(port.getText().toString()));
             }
